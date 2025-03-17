b492d19e5def3e37631418978d60b6c9f9c4d3a4
Maribel Hernandez <codebymaribel@gmail.com>
2025-03-17 17:50:05 -0400

    [Add] Use name-to-imdb package to get the IMDB ID of a Film
    Why is it necessary?
     - The film Object has an optional ID for the IMDB id.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import nameToImdb from "name-to-imdb";
+
 /**
  * @description Checks if a string is a valid Letterboxd URL
  * @param {String} url - Letterboxd URL
@@ -13,6 +15,17 @@ export const measureFunctionTime = async (fn, args) => {
   return result;
 };
 
-export async function timeout(ms:number) {
+export async function timeout(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export const searchIMDB = (title: string) => {
+  return new Promise((resolve, reject) =>
+    nameToImdb(title, (err, res) => {
+      if (err) reject(err);
+      else resolve(res);
+    }),
+  )
+    .then((imdbID) => imdbID)
+    .catch(() => null);
+};

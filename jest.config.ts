7e7a245e35b840b61f00d85bec7508ebb9bcf78e
Maribel Hernandez <codebymaribel@gmail.com>
2025-03-23 17:14:48 -0400

    [Add] Jest config

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest", // Use ts-jest for TypeScript
+  testEnvironment: "node", // Run tests in a Node.js environment
+  testMatch: ["**/tests/**/*.test.ts"], // Look for test files in the `tests` folder
+  collectCoverage: true, // Enable code coverage
+  coverageDirectory: "coverage", // Output coverage reports to the `coverage` folder
+  verbose: true, // Show detailed test results
+};
+
+export default config;

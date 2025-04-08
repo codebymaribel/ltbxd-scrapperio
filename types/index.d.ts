f236bad6fed8c2f2f3bb2669f81a46acb0149d10
Maribel Hernandez <33031120+codebymaribel@users.noreply.github.com>
2025-04-08 11:54:02 -0400

    Revert "Develop"

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,38 +1,18 @@
-import { QUERY_RESULT_STATUS } from '../config/constants';
-import { FilmObject } from './films';
-import { ListCoverObject } from './lists';
+import { QUERY_RESULT_STATUS } from "../config/constants";
+import { FilmObject } from "./films";
 
 export type OptionsProps = {
   poster?: boolean;
   IMDBID?: boolean;
   max?: number;
 };
-
 export interface UserQueryProps {
   username: string;
   options?: OptionsProps;
 }
 
-export type FilmsResponseProps = {
+export type QueryResponseProps = {
   status: (typeof QUERY_RESULT_STATUS)[keyof typeof QUERY_RESULT_STATUS];
   data: FilmObject[] | [];
   errorMessage: string | null;
-};
-
-export type ListsResponseProps = {
-  status: (typeof QUERY_RESULT_STATUS)[keyof typeof QUERY_RESULT_STATUS];
-  data: ListCoverObject[] | [];
-  errorMessage: string | null;
-};
-
-export type UserListsOptions = {
-  posters?: boolean;
-  summary?: boolean;
-  amount?: boolean;
-  max?: number;
-};
-
-export type UserListsProps = {
-  username: string;
-  options?: UserListsOptions;
 };
--- types/lists.d.ts
+++ types/lists.d.ts
@@ -1,5 +1,5 @@
-import { OptionsProps, UserListsOptions } from '.';
-import { FilmObject } from './films';
+import { OptionsProps } from ".";
+import { FilmObject } from "./films";
 
 export type ListScrapperProps = {
   url: string;
@@ -7,28 +7,8 @@ export type ListScrapperProps = {
   options?: OptionsProps;
 };
 
-export type UserListsProps = {
-  url: string;
-  totalItems?: number;
-  options?: UserListsOptions;
-};
-
 export type ScrappedList = {
   films: FilmObject[] | [];
   nextPageUrl: string | null;
   error: string | null;
-};
-
-export type ScrappedLists = {
-  lists: ListCoverObject[] | [];
-  nextPageUrl: string | null;
-  error: string | null;
-};
-
-export type ListCoverObject = {
-  title: string;
-  summary?: string | null;
-  amount?: string | null;
-  posters?: string[] | null;
-  url: string;
 };
